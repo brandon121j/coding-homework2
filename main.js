86092d1a19bb107f39ada20216c292edb04e93ff
Brandon Johnson <brandon121j@gmail.com>
2022-01-31 21:39:20 -0500

    Completion of assignment

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
 
-const subtract = Document.getElementById('subtract');
-const add = Document.getElementById('add');
-const counter = Document.getElementById
+const counter = document.getElementById('counter');
+const subtract = document.getElementById('subtract');
+const add = document.getElementById('add');
+const starDiv = document.getElementById('stars');
+
+let counterVal = 0;
+
+add.addEventListener('click', () => {
+    counterVal++;
+    counter.innerHTML = counterVal;
+    starDiv.appendChild(stars());
+});
+
+subtract.addEventListener('click', () => {
+    counterVal--;
+    counter.innerHTML = counterVal;
+    try {
+        starDiv.removeChild(starDiv.lastElementChild)
+    } catch(e) {}
+});
+
+const stars = () => {
+    const appender = document.createElement('div');
+    appender.innerHTML = `<img class='star' alt='star image' src='./star.png'/>`;
+    return appender
+}
+
